c23a7d7d425481c6d9fb073101812bbbd6ca00e1
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-09 20:48:56 +0300

    Update TaskB5.cs

--- Задачки/Homework/Level B/TaskB5.cs
+++ Задачки/Homework/Level B/TaskB5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,13 +19,38 @@ namespace Homework
     // + a      +
     // + frame  +
     // ++++++++++
+    
     public static class TaskB5
     {
+        public static int Max(List<string> text)  //это для поиска самого длинного элемента списка
+        {
+            int max = 0;
+            for (int i = 0; i < text.Count; i++)
+            {
+                if (text[i].Length > max)
+                {
+                    max = text[i].Length;
+                }
+            }
+            return max;
+        }
         public static string Frame(List<string> text, char symbol)
         {
-            // Здесь необходимо написать код.
+            int max = Max(text);
+            string str;
 
-            return "";
+            for (int i = 0; i < text.Count; i++)
+            {
+                str = new string(' ', max - text[i].Length);
+                text[i] = symbol + " " + text[i] + str + " " + symbol;
+            }
+
+            str = new string(symbol, Max(text));
+            text.Add(str);
+            text.Insert(0, str);
+
+            string ans = string.Join("\n", text.ToArray());
+            return ans;
         }
     }
 }
