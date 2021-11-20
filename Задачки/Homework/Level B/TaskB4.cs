f62c5107f7606418dc8458f5e6b36cf56f4dd5d3
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-20 12:09:00 +0300

    Update TaskB4.cs

--- Задачки/Homework/Level B/TaskB4.cs
+++ Задачки/Homework/Level B/TaskB4.cs
@@ -17,11 +17,79 @@ namespace Homework
     // CheckBrackets("(abc)]{0:1}[") ==> false.
     public static class TaskB4
     {
-        public static bool CheckBrackets(string s)
+      public static bool CheckBrackets(string s)
         {
             // Здесь необходимо написать код.
-
-            return false;
+            int lenght = s.Length;
+            int check = 0;
+            bool flag = true;
+            for (int i = 0; i < lenght; i++)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                        for (int j = i; j < lenght; j++)
+                        {
+                            if (s[j] == ')')
+                            {
+                                check = 1;
+                                break;
+                            }
+                        }
+                        if (check == 0)
+                        {
+                            flag = false;
+                        }
+                        check = 0;
+                        break;
+                    case '[':
+                        for (int j = i; j < lenght; j++)
+                        {
+                            if (s[j] == ']')
+                            {
+                                check = 1;
+                                break;
+                            }
+                        }
+                        if (check == 0)
+                        {
+                            flag = false;
+                        }
+                        check = 0;
+                        break;
+                    case '{':
+                        for (int j = i; j < lenght; j++)
+                        {
+                            if (s[j] == '}')
+                            {
+                                check = 1;
+                                break;
+                            }
+                        }
+                        if (check == 0)
+                        {
+                            flag = false;
+                        }
+                        check = 0;
+                        break;
+                    case '<':
+                        for (int j = i; j < lenght; j++)
+                        {
+                            if (s[j] == '>')
+                            {
+                                check = 1;
+                                break;
+                            }
+                        }
+                        if (check == 0)
+                        {
+                            flag = false;
+                        }
+                        check = 0;
+                        break;
+                }
+            }
+            return flag;
         }
     }
 }
